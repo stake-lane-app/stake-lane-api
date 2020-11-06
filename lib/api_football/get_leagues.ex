828a196822966b1de68ec830be557c53efff7087
Lucas Alves <lucashalves@live.com>
2020-11-06 19:46:30 +0100

    Isolating getleagues third party logic

--- lib/api_football/get_leagues.ex
+++ lib/api_football/get_leagues.ex
@@ -1,6 +1,6 @@
 defmodule ApiFootball.GetLeagues do
 
-  def get_league_by_league_id(api_football_league_id) do
+  def get_league_id(api_football_league_id) do
     envs = Application.fetch_env!(:bolao_hub_api, :football_api)
     headers = ["X-RapidAPI-Key": envs[:key]]
 
@@ -11,5 +11,22 @@ defmodule ApiFootball.GetLeagues do
       |> (&(&1["api"]["leagues"])).()
       |> Enum.find(&(&1["league_id"] == api_football_league_id))
   end
+
+  def parse_league_to_update(refreshed_league) do
+    refreshed_season_start = refreshed_league["season_start"] |> Date.from_iso8601!
+    refreshed_season_end = refreshed_league["season_end"] |> Date.from_iso8601!
+    is_active = Date.utc_today() |> Date.diff(refreshed_season_end) |> is_league_active()
+
+    refreshed_attrs = %{
+      is_active: is_active,
+      season_start: refreshed_season_start,
+      season_end: refreshed_season_end,
+    }
+
+    refreshed_attrs
+  end
+
+  defp is_league_active(diff_date) when diff_date <= 0, do: true
+  defp is_league_active(_), do: false
 
 end
--- lib/bolao_hub_api/workers/update_leagues.ex
+++ lib/bolao_hub_api/workers/update_leagues.ex
@@ -14,36 +14,13 @@ defmodule BolaoHubApi.Workers.UpdateLeagues do
   def perform(%Oban.Job{}) do
     done = @third_api
     |> League.list_active_leagues_by_third_api
-    |> Enum.map(&request_league(&1))
-    |> Enum.map(&update_league(&1))
+    |> Enum.map(fn league ->
+      league.third_party_info["league_id"]
+      |> GetLeagues.get_league_id
+      |> GetLeagues.parse_league_to_update
+      |> (&(League.update_league(league, &1))).()
+    end)
 
     { :ok, done }
   end
-
-  defp request_league(league) do
-    %{
-      "actual_league" => league,
-      "refreshed_league" => league.third_party_info["league_id"] |> GetLeagues.get_league_by_league_id(),
-    }
-  end
-
-  defp update_league(league) do
-    refreshed_season_start = Date.from_iso8601!(league["refreshed_league"]["season_start"])
-    refreshed_season_end = Date.from_iso8601!(league["refreshed_league"]["season_end"])
-
-    is_active = Date.utc_today()
-      |> Date.diff(refreshed_season_end)
-      |> is_league_active()
-
-    refreshed_attrs = %{
-      is_active: is_active,
-      season_start: refreshed_season_start,
-      season_end: refreshed_season_end,
-    }
-
-    { :ok, _ } = League.update_league(league["actual_league"], refreshed_attrs)
-  end
-
-  defp is_league_active(diff_date) when diff_date <= 0, do: true
-  defp is_league_active(_), do: false
 end
