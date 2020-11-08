77281d4aac4c29f767a226e1129a3b0a0aedbe48
Lucas Alves <lucashalves@live.com>
2020-11-08 22:49:26 +0100

    Querying the user teams correctly

--- lib/bolao_hub_api/user_team.ex
+++ lib/bolao_hub_api/user_team.ex
@@ -8,12 +8,21 @@ defmodule BolaoHubApi.UserTeam do
   alias BolaoHubApi.Links.UserTeam
 
   def get_user_teams(user_id) do
-    query = from ut in UserTeam,
-      inner_join: team in assoc(ut,  :team),
-      where: ut.user_id == ^user_id,
-      preload: [
-        team: team,
-      ]
+    query = from user_team in UserTeam,
+      inner_join: team in assoc(user_team,  :team),
+      inner_join: country in assoc(team, :team),
+      where: user_team.user_id == ^user_id,
+      select: %{
+        level_type: user_team.level_type,
+        team_id: team.team_id,
+        name: team.name,
+        full_name: team.full_name,
+        logo: team.logo,
+        is_national: team.is_national,
+        founded: team.founded,
+        venue: team.venue,
+        country: country,
+      }
 
     Repo.all query
   end
